7c2121f62aaf6037b4457422eadb48fa4f8d5cd4
Alexandre Foggiatto <a.foggiatto@rs.tus.ac.jp>
2022-05-08 22:02:18 +0900

    Start project

---
+++ mypca.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+
+class SimplePCA:
+    def __init__(self, n_components=2):
+        self.num_components = n_components
+        self.x_std = np.array([0])
+
+    def fit_transform(self, X):
+        """Fit and the model with X and apply the dimension reduction in X.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+        Returns
+        -------
+        project :  ndarray of shape (n_samples, n_components)
+            Returns the transformed X (PCs)
+        """
+        # Update variable x_std with the standardization
+        self.get_standardization(X)
+        # Apply the PCA steps
+        explanation, pcs = self.get_eigen()
+        project = self.project_matrix(pcs)
+        self.get_variance(project)
+
+        return project
+
+    def get_covariant(self):
+        """Calculated the covariant matrix of X.
+
+        Parameters
+        ----------
+        self : object
+        Returns
+        -------
+        covariant :  ndarray of shape (n_samples, n_samples)
+            Returns a square covariant matrix
+        """
+        covariant = np.dot(self.x_std.T, self.x_std)
+        return covariant
+
+    def get_standardization(self, X, stand=False):
+        """Centralize and standize the data.
+        Parameters
+        ----------
+        X: array-like of shape (n_samples, n_features)
+            The data input.
+        stand: boolean, default=False
+            If True, standarize the input data.
+        Returns
+        -------
+        covariant :  ndarray of shape (n_samples, n_samples)
+            Returns a square covariant matrix
+        """
+        mean = np.mean(X, axis=0)
+        if stand:
+            std = np.std(X, axis=0)
+            x_std = (X - mean)/ std
+        else:
+            x_std = (X - mean)
+        self.x_std = x_std
+
+    def get_eigen(self):
+        """Calculate the eigenvalue and eigenvectors
+        Parameters
+        ----------
+        self : object.
+        Returns
+        -------
+        explanation :  ndarray of shape (n_components,)
+            Ordered variance
+         pcs: ndarray of shape (n_components, n_features)
+            Principal components
+        """
+        eigenvalues, eigenvectors = np.linalg.eigh(self.get_covariant())
+
+        projection = self.project_matrix(eigenvectors)
+        variance = self.get_variance(projection)
+        # Order based on the variance
+        v_components = np.argsort(variance)[::-1][:self.num_components]
+        # The principal components are the transposed eigenvectors.
+        pcs = eigenvectors[:, v_components].T
+        explanation = variance[v_components]
+        return explanation, pcs
+
+    def project_matrix(self, eigenvectors):
+        """Calculate the projection of X in the eigenvectors"""
+        return np.dot(self.x_std, eigenvectors.T)
+
+    def get_variance(self, projection):
+        """Calculate variance"""
+        variance = np.var(projection, axis=0, ddof=1)
+        return variance
---
+++ test.py
@@ -0,0 +1,17 @@
+import mypca
+import numpy as np
+
+x = np.array([[-1, -1, 1], [-2, -1, 1], [-3, -2, 2], [1, 1, 3], [2, 1, 1], [3, 2, 9]])
+output = np.array([[2.29548456, 0.14307812],
+                   [2.8334206, -0.5155151],
+                   [2.97010772, -2.2054038],
+                   [-1.02560371, 0.93513766],
+                   [-0.03968273, 2.88758981],
+                   [-7.03372643, -1.24488669]])
+
+pca = mypca.SimplePCA()
+transformation = pca.fit_transform(x)
+if transformation == np.round(output, 2):
+    print("mypca is working!!")
+else:
+    print("The result is different from expected, please check the again the implementation")
